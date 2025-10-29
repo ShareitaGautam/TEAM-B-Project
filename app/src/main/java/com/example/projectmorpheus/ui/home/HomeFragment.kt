f87713283aa93e6071d890f2dc71fff34028fd38
William Reynolds-Uti <reynoldsuw1@nku.edu>
2025-10-29 12:19:44 -0400

    Create homescreen clock and progress bar

--- app/src/main/java/com/example/projectmorpheus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/projectmorpheus/ui/home/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.example.projectmorpheus.ui.home
 
 import android.os.Bundle
+import android.os.Handler
+//import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.example.projectmorpheus.data.AlarmDatabase
 import com.example.projectmorpheus.databinding.FragmentHomeBinding
+import java.util.Calendar
 
 class HomeFragment : Fragment() {
 
@@ -22,16 +28,44 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val hourHand: ImageView = binding.hourhand
+        val minuteHand: ImageView = binding.minutehand
+        val secondHand: ImageView = binding.secondhand
+        val alarmProgress: ProgressBar = binding.alarmprogress
+        val alarmDao = AlarmDatabase.getDatabase(requireContext()).alarmDao()
+        //run whenever a second passes
+        homeViewModel.secondDegrees.observe(viewLifecycleOwner) {
+            secondHand.rotation = it.toFloat()
+            minuteHand.rotation = homeViewModel.minuteDegrees.value!!
+            hourHand.rotation = homeViewModel.hourDegrees.value!!
+
+            val currentTime = Calendar.getInstance()
+            alarmDao.getAllAlarms().asLiveData().observe(viewLifecycleOwner) { alarms ->
+                if (alarms.isNotEmpty()) {
+                    val targetMinute = alarms[0].hourOfDay * 60 + alarms[0].minute
+                    val dayMinute = currentTime.get(Calendar.HOUR_OF_DAY) * 60 + currentTime.get(Calendar.MINUTE)
+                    alarmProgress.progress = (dayMinute - targetMinute).mod(1440)
+                } else {
+                    alarmProgress.progress = 0
+                }
+            }
         }
+
+        //https://stackoverflow.com/questions/55570990/kotlin-call-a-function-every-second
+        val mainHandler = Handler()
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                mainHandler.postDelayed(this, 1000)
+                homeViewModel.updatetime()
+            }
+        })
+
+
         return root
     }
 
--- app/src/main/java/com/example/projectmorpheus/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/projectmorpheus/ui/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.projectmorpheus.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.Calendar
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +11,21 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+
+    val secondDegrees = MutableLiveData<Float>().apply {
+        value = (6.0*Calendar.getInstance().get(Calendar.SECOND)).toFloat() }
+    val minuteDegrees = MutableLiveData<Float>().apply {
+        value = (6.0*Calendar.getInstance().get(Calendar.MINUTE)).toFloat() + secondDegrees.value!!/60 }
+    val hourDegrees = MutableLiveData<Float>().apply {
+        value = (15.0*Calendar.getInstance().get(Calendar.HOUR)).toFloat() + minuteDegrees.value!!/60 }
+
+    fun updatetime() {
+        val currentTime = Calendar.getInstance()
+        secondDegrees.value = (6.0*currentTime.get(Calendar.SECOND)).toFloat()
+        minuteDegrees.value = (6.0*currentTime.get(Calendar.MINUTE)).toFloat() + secondDegrees.value!!/60
+        hourDegrees.value = (15.0*currentTime.get(Calendar.HOUR)).toFloat() + minuteDegrees.value!!/60
+
+    }
+
 }
